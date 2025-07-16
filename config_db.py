a58dacb7965075846536de85a8f2bb421ddea9c1
Stormy123456 <72662635+Stormy123456@users.noreply.github.com>
2025-07-16 14:39:41 +0700

    Update config_db.py

--- config_db.py
+++ config_db.py
@@ -1,3 +1,3 @@
-connection_string = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=192.168.10.136;PORT=5002;DATABASE=odoo_tg_golive_99;UID=tgfonereadonly08;PWD=WS2NZuPsnrTkSwWU"
-connection_string_invoice = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=localhost;PORT=5432;DATABASE=invoice_from_excel;UID=postgres;PWD=1234"
-db_test = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=localhost;PORT=5432;DATABASE=db_test_promotion;UID=postgres;PWD=1234"
+connection_string = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=xxx.xxx.xx.xxx;PORT=5002;DATABASE=odoo_tg_golive_99;UID=tgfonereadonly08;PWD=WS2NZuPsnrTkSwWU"
+connection_string_invoice = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=localhost;PORT=xxxx;DATABASE=invoice_from_excel;UID=postgres;PWD=1234"
+db_test = "DRIVER={PostgreSQL ODBC Driver(UNICODE)};SERVER=localhost;PORT=xxxx;DATABASE=db_test_promotion;UID=postgres;PWD=1234"
